4c204def886d5b21b74fb423036d7cfbe3b6fef0
Rajas1312 <you@example.com>
2020-12-14 23:31:34 +0530

    Added:runing till the winning position is achived

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,36 +4,63 @@ public class SnakeAndLadder {
 
 	static int NUMBER_OF_PLAYER=1;
 	static int INITIAL_POSITION=0;
+	static int WINNING_POSITION=100;
+	int position=0;
 	
 	public static int getRoll() {
 		
 	int roll=(int)(Math.random()*10%6+1);
 	return roll;
 	}
-	public void checkOption() {
+	
+public  void checkOption() {
+		
 		
 		System.out.println("Selecting the option of play randomly");
+		while(this.position!=WINNING_POSITION) {
 		int option=(int)(Math.random()*10%3+1);
 		switch(option) {
 		
 		case 1:
 			System.out.println("No play");
-			System.out.println(INITIAL_POSITION);
-			
+			this.position=this.position+INITIAL_POSITION;
+			if(this.position>=0) {
+			System.out.println(this.position);
+			}
+			else {
+				this.position=INITIAL_POSITION;
+			}
 			break;
 		case 2:
 			System.out.println("ladder encountered");
 			int ladder=INITIAL_POSITION+getRoll();
-			System.out.println(ladder);
+			this.position=this.position+ladder;
+			if(this.position>=0 ) {
+				System.out.println(this.position);
+			}
+			else {
+				this.position=INITIAL_POSITION;
+				System.out.println(INITIAL_POSITION);
+			}
 			break;
-			
 		case 3:
 			System.out.println("Snake encountred");
 			int snake=INITIAL_POSITION-getRoll();
-			System.out.println(snake);
+			this.position=this.position +snake;
+			if (this.position>=0) {
+				System.out.println(this.position);
+			}
+			else {
+				this.position=INITIAL_POSITION;
+				System.out.println(INITIAL_POSITION);
+			}
 			break;
+			}
+			
 		}
+			
 	}
+	
 	
 	public static void main(String[] args) {
 		
