55f131f5e991f96213ad3efd2e250aefebf7a9b5
Rajas1312 <you@example.com>
2020-12-14 22:53:05 +0530

    Added:the roll of the die

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,6 +5,12 @@ public class SnakeAndLadder {
 	static int NUMBER_OF_PLAYER=1;
 	static int INITIAL_POSITION=0;
 	
+	public static int getRoll() {
+		
+	int roll=(int)(Math.random()*10%6+1);
+	return roll;
+	}
+	
 	public static void main(String[] args) {
 		
 	System.out.println("welcome to snake and ladder");
