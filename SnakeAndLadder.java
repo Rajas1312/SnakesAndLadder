bad859873320e79cfe261d85562e47f22d396d15
Rajas1312 <you@example.com>
2020-12-14 23:15:47 +0530

    Added:the options of all the possible moves

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -10,11 +10,36 @@ public class SnakeAndLadder {
 	int roll=(int)(Math.random()*10%6+1);
 	return roll;
 	}
+	public void checkOption() {
+		
+		System.out.println("Selecting the option of play randomly");
+		int option=(int)(Math.random()*10%3+1);
+		switch(option) {
+		
+		case 1:
+			System.out.println("No play");
+			System.out.println(INITIAL_POSITION);
+			
+			break;
+		case 2:
+			System.out.println("ladder encountered");
+			int ladder=INITIAL_POSITION+getRoll();
+			System.out.println(ladder);
+			break;
+			
+		case 3:
+			System.out.println("Snake encountred");
+			int snake=INITIAL_POSITION-getRoll();
+			System.out.println(snake);
+			break;
+		}
+	}
 	
 	public static void main(String[] args) {
 		
 	System.out.println("welcome to snake and ladder");
-
+	SnakeAndLadder snakeAndLadder=new SnakeAndLadder();
+	snakeAndLadder.checkOption();
 	}
 	
 }
